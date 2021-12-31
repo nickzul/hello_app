f9236ca8381434737012aacf3985eadbc4510359
nickzul <nzulcic@gmail.com>
2021-12-30 21:23:35 -0800

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # To change the default route to a custom. (sintax: application_controller#action_name)
+  root 'application#hello'
+end
